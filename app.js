3ce69e68fc802a2002abbc9febd7001a4d8849c3
Unics Yu <yulele28@126.com>
2023-05-15 20:39:32 -0700

    [feat] save progress

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 const fs = require("fs")
 
-const dataSetName = "controversialAll1000"
+const dataSetName = "test"
 
 function collectData(sort, limit, after) {
     if (sort != "") sort = `/${sort}`
@@ -18,28 +18,38 @@ function collectData(sort, limit, after) {
 
 let dataRes = []
 
+let next = ""
+
+// Recover progress from json file
+if (fs.existsSync(`./data/${dataSetName}.json`)) {
+    dataRes = JSON.parse(fs.readFileSync(`./data/${dataSetName}.json`))
+    console.log("Recovered", dataRes.length)
+    next = dataRes[dataRes.length - 1].data.name
+}
+
 
 function collectDataRecursive(sort, limit, after) {
     return new Promise((resolve, reject) => {
         collectData(sort, limit, after).then((res) => {
-            console.log("Finished", res.data.data.after)
-            if(!res.data.data.after){
+            dataRes = dataRes.concat(res.data.data.children)
+            console.log("Finished", res.data.data.after, res.data.data.children.length)
+            fs.writeFileSync(`./data/${dataSetName}.json`, JSON.stringify(dataRes, null, 2))
+            if (!res.data.data.after) {
                 console.log("ALERT: No more data")
                 resolve(res)
                 return;
             }
-            dataRes = dataRes.concat(res.data.data.children)
             if (limit > 100)
                 collectDataRecursive(sort, limit - 100, res.data.data.after).then(res => {
                     resolve(res)
                 })
-            else{
+            else {
                 resolve(dataRes)
             }
         })
     })
 }
 
-collectDataRecursive("controversial", 1000, "").then(res => {
+collectDataRecursive("", 1000, next).then(res => {
     fs.writeFileSync(`./data/${dataSetName}.json`, JSON.stringify(dataRes, null, 2))
 })
